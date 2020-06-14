4fd8abd466ff61919904d3ec62aa09c109994e44
Luiz Faria <faria.luiz@gmail.com>
2020-06-14 17:46:00 -0300

    poc com o FluentValidation

---
+++ POCFluentValidation/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POCFluentValidation.Model;
+using POCFluentValidation.Validators;
+using System.Threading.Tasks;
+
+namespace POCFluentValidation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Running!");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Person person)
+        {
+            var validator = new PersonValidator();
+
+            var validationResult = validator.Validate(person);
+
+            if (!validationResult.IsValid)
+                return BadRequest(validationResult.Errors);
+
+            return Ok();
+        }
+    }
+}
---
+++ POCFluentValidation/Validators/PersonValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using POCFluentValidation.Model;
+using System;
+using System.Linq;
+
+namespace POCFluentValidation.Validators
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(person => person.Name)
+                .NotEmpty().WithMessage("Nome não pode ser vazio.")
+                .Must(IsValidName).WithMessage("Nome deve ser composto somente de letras.");
+
+            RuleFor(person => person.Email)
+                .NotEmpty().WithMessage("Email não pode ser vazio.")
+                .EmailAddress().WithMessage("Endereço de email informado não é um endereço válido.");
+
+            RuleFor(person => person.BirthDate)
+                .NotEmpty().WithMessage("Data de nascimento deve ser informada.")
+                .LessThan(DateTime.Now).WithMessage("Data de nascimento não pode ser superior a data atual");
+        }
+
+        private bool IsValidName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return true;
+
+            return !name.All(Char.IsDigit);
+        }
+    }
+}
